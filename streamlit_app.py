8edfbcb3efc7a680ba71894ab3c40bd78215b7e8
BIvana07 <blessing.hassan@strathmore.edu>
2024-08-25 14:07:37 +0000

    I added code from tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-import pickle
+import pickle  
 import numpy as np
 import streamlit as st
 
